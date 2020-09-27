fcaaf3e5186225dd898d505a100517f887f4aa27
riadh1993 <riadhkar1993@gmail.com>
2020-09-27 09:58:53 +0100

    first commit

---
+++ test.java
@@ -0,0 +1,46 @@
+public class SumOfOverlappingElements {
+
+    public static void findOverlappingElements(int [] setA, int [] setB){
+
+        System.out.println("Set 1: " + Arrays.toString(setA) + ", Set 2: " + Arrays.toString(setB));
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i <setA.length ; i++) {
+            int element = setA[i];
+            if(map.containsKey(element)) {
+                int count = map.get(element);
+                map.put(element, count+1);
+            }else
+                map.put(element, 1);
+        }
+
+        for (int i = 0; i <setB.length ; i++) {
+            int element = setB[i];
+            if(map.containsKey(element)) {
+                int count = map.get(element);
+                map.put(element, count+1);
+            }else
+                map.put(element, 1);
+        }
+
+        //get sum of distinct elements
+        int sum = 0;
+        Set<Integer> set = map.keySet();
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()){
+            int key = iterator.next();
+            int count = map.get(key);
+            if(count==2)
+                sum += key*count;
+        }
+        System.out.println("Sum of overlapping elements: " + sum);
+        System.out.println("----------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        int A[] = {8, 6, 1, 2};
+        int B[] = {8, 5, 6, 7};
+        findOverlappingElements(A, B);
+        
+    }
+}
